26b09676bc3eea287f0a653da1ea12453513d98a
Jose Ramon Martinez Riveiro <Josera.martinez@hotmail.com>
2024-12-06 01:00:49 +0100

    fix:UI experience improved with toast and loading states

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -5,14 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   // @ts-ignore
   env: {
     schema: {
-      API_SERVER: envField.string({context: 'server', access: 'secret'}),
-      API_KEY: envField.string({context: 'server', access: 'secret'})
+      API_SERVER: envField.string({context: 'client', access: 'public'}),
     }
   },
 });
--- webapp/src/components/Form.jsx
+++ webapp/src/components/Form.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'preact/hooks';
 import Input from './Input.jsx';
 import Card from './Card.jsx';
-
+import Toast from './Toast.jsx';
+import { API_SERVER } from 'astro:env/client';
 
 export default function Form() {
   const [origin, setOrigin] = useState('');
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [toast, setToast] = useState(null);
 
   useEffect(() => {
     const savedCards = JSON.parse(localStorage.getItem('cards')) || [];
@@ -19,40 +21,66 @@ export default function Form() {
     }
   }, [cards]);
 
+  const showToast = (message) => {
+    setToast(message);
+  };
 
   const handleSubmit = async (url) => {
     setLoading(true);
 
-    const response = await fetch('/api/shorten', {
-      method: 'POST',
-      headers: { 
-        'Content-Type': 'application/json',
-       },
-      body: JSON.stringify({ origin: url }),
-    });
+    try {
+      const response = await fetch(`${API_SERVER}/api/shorten`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ origin: url }),
+      });
 
-    if (!response.ok) {
-      throw new Error('Failed to shorten URL');
+      if (!response.ok) {
+        
+        showToast(
+          {
+            message: 'An error occurred while shortening the URL. Please try again later.',
+            type: 'error'
+          }
+        );        setLoading(false);
+        return;
+      }
+
+      const data = await response.json();
+      setCards((prevCards) => [
+        { href: data.href, origin: url },
+        ...prevCards,
+      ]);
+
+      setLoading(false);
+      setOrigin('');
+      showToast(
+        {
+          message: 'URL shortened successfully!',
+          type: 'success'
+        }
+      );
+    } catch (error) {
+      setLoading(false);
+      showToast(
+        {
+          message: 'An error occurred while shortening the URL. Please try again later.',
+          type: 'error'
+        }
+      );
     }
-
-    const data = await response.json();
-    setCards((prevCards) => [
-      { href: data.href, origin: url },
-      ...prevCards,
-    ]);
-
-    setLoading(false);
-    setOrigin('');
   };
 
   return (
     <section class="w-full max-w-lg rounded-lg p-6 mb-12">
       <Input
-            origin={origin}
-            setOrigin={setOrigin}
-            onSubmit={handleSubmit}
-            loading={loading}
-          />
+        origin={origin}
+        setOrigin={setOrigin}
+        onSubmit={handleSubmit}
+        loading={loading}
+      />
 
       {cards.length > 0 && (
         <div>
@@ -61,6 +89,8 @@ export default function Form() {
           ))}
         </div>
       )}
+
+      {toast && <Toast message={toast.message} type={toast.type} onClose={() => setToast(null)} />}
     </section>
   );
 }
